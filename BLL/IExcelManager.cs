1313a660f0c048b1562958170aaaee6d47c0f532
lwm <luweimin1015@foxmail.com>
2016-11-30 22:18:51 +0800

    框架优化

--- BLL/IExcelManager.cs
+++ BLL/IExcelManager.cs
@@ -5,9 +5,19 @@ using System.Text;
 
 namespace DAPACS.BLL
 {
+    #region
+    /**
+     * 业务逻辑层接口
+     * 这里主要处理业务逻辑，比如对Excel的处理，需要调用DAL层获取
+     * Excel对象，然后在该层做一些操作（输出Excel的行数，列数等等）
+     * 
+     */
+    #endregion
     interface IExcelManager
     {
         //默认公有方法
         void test();
+
+        int getSheetCount();
     }
 }
--- Util/LogUtil.cs
+++ Util/LogUtil.cs
@@ -5,6 +5,13 @@ using System.Text;
 
 namespace DAPACS.Util
 {
+    #region
+    /**
+     * 日志工具类
+     * 主要提供记录日志的公共方法，如输出到控制台，输出到文件日志等。
+     * 
+     */
+    #endregion
     class LogUtil
     {
         public static void writeConsole(String msg)
