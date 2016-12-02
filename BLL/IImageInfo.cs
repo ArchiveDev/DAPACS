d1bd4a3b17c5c74774d76fbc25576e3aa3a33db2
ArtanisW <wjqwonder@hotmail.com>
2016-12-02 23:18:31 +0800

    图片信息读取0.9
    已初步完成图片信息读取，对于压缩率及色彩模式有待优化

---
+++ BLL/IImageInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DAPACS.BLL
+{
+    interface IImageInfo
+    {
+        //获取像素尺寸
+        int GetWidth(string pathname);
+        int GetHeight(string pathname);
+        //获取分辨率
+        float GetDpiX(string pathname);
+        float GetDpiY(string pathname);
+        //获取色彩模式
+        string GetColorMode(string pathname);
+        //获取图片格式
+        string GetImageFormat(string pathname);
+    }
+}
