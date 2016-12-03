4e7a51a95f98b36e642f870df309ea6cc887023b
arhan19 <2292025880@qq.com>
2016-12-03 10:31:58 +0800

    音视频信息读取
    依赖了第三方dll库，运行时需要将MediaInfo.dll放至exe用文件夹下

---
+++ AVInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MediaInfoNET;
+
+namespace DAPACS.BLL
+{
+    interface AVInfo
+    {
+        //获取音视频文件名
+        string GetAVFileName(string pathname);
+        //获取音视频文件格式
+        string GetAVFileFormat(string pathname);
+        //获取音视频文件时长
+        string GetAVFileDuration(string pathname);
+        //获取音视频文件大小
+        string GetAVFileSize(string pathname);
+        //获取音频比特率
+        string GetAudioBitrate(string pathname);
+        //获取音频采样率
+        string GetAudioSimplingRate(string pathname);
+        //获取视频码率
+        string GetVideoBitrate(string pathname);
+    }
+}
